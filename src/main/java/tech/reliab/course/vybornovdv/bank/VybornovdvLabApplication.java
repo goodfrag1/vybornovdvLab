d20a0e9ea48c829f5a65034db4a873d0f6591b7f
vybornovdv <GiGaBiT2017@yandex.ru>
2024-12-10 19:37:18 +0300

    fixed swagger web ui

--- src/main/java/tech/reliab/course/vybornovdv/bank/VybornovdvLabApplication.java
+++ src/main/java/tech/reliab/course/vybornovdv/bank/VybornovdvLabApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VybornovdvLabApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(VybornovdvLabApplication.class, args);
+
     }
 }
